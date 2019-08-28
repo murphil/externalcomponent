773b5401964b6f2d0f0e317562a242e7dc2e6013
murphy <murphy@iffy.me>
2019-08-28 16:54:02 +0800

    loadExt

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+function loadExt(name) {
+  if (window[name]) return window[name];
+
+  window[name] = new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.addEventListener('load', () => {
+      resolve(window[name]);
+    });
+    script.addEventListener('error', () => {
+      reject(new Error(`Error loading ${name}`));
+    });
+    script.src = `${baseUrl}/${name.split('.')[0]}/${name}.umd.min.js`;
+    document.head.appendChild(script);
+  });
+
+  return window[name];
+}
+
 export default function externalComponent(baseUrl) {
   return function (name) {
-    return function () {
-      if (window[name]) return window[name];
-
-      window[name] = new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.async = true;
-        script.addEventListener('load', () => {
-          resolve(window[name]);
-        });
-        script.addEventListener('error', () => {
-          reject(new Error(`Error loading ${name}`));
-        });
-        script.src = `${baseUrl}/${name.split('.')[0]}/${name}.umd.min.js`;
-        document.head.appendChild(script);
-      });
-
-      return window[name];
-    }
+    return () => loadExt(name)
   }
 }
 
