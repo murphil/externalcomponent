447306c45332536192a3a4de16b47e1d76304aee
murphy <murphy@iffy.me>
2019-08-28 16:48:11 +0800

    1.0.2

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 export default function externalComponent(baseUrl) {
-  return async function (name) {
-    if (window[name]) return window[name];
+  return function (name) {
+    return function () {
+      if (window[name]) return window[name];
 
-    window[name] = new Promise((resolve, reject) => {
-      const script = document.createElement('script');
-      script.async = true;
-      script.addEventListener('load', () => {
-        resolve(window[name]);
+      window[name] = new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.async = true;
+        script.addEventListener('load', () => {
+          resolve(window[name]);
+        });
+        script.addEventListener('error', () => {
+          reject(new Error(`Error loading ${name}`));
+        });
+        script.src = `${baseUrl}/${id.split('.')[0]}/${id}.umd.min.js`;
+        document.head.appendChild(script);
       });
-      script.addEventListener('error', () => {
-        reject(new Error(`Error loading ${name}`));
-      });
-      script.src = `${baseUrl}/${id.split('.')[0]}/${id}.umd.min.js`;
-      document.head.appendChild(script);
-    });
 
-    return window[name];
+      return window[name];
+    }
   }
 }
 
